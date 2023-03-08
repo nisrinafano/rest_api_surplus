d0573725da8afc7b093308947cff680b21562d5d
nisrinafano <19148635+nisrinafano@users.noreply.github.com>
2023-03-08 11:33:44 +0700

    Set database
    - Add migration file for database
    - Remove unused files

---
+++ database/migrations/2023_03_08_042756_create_product_image_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductImageTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_image', function (Blueprint $table) {
+            $table->integer('product_id');
+            $table->integer('image_id');
+            $table->index('product_id');
+            $table->index('image_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_image');
+    }
+}
